48842d66f97546bf2ad7c760a9df7728fe9d3c1d
Erasmus Hadebe <erasmus.hadebe@detegoglobal.com>
2025-04-04 14:54:45 +0200

    Added data.js

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+
+
+const data = [
+    {
+        id: 1,
+        dishName: "Greek Salad",
+        price: "R 129.99",
+        image: require('../icons_assets/greek salad.jpg'),
+        description: "The famous greek salad of crispy lettuce,peppers,olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary cruotons."
+    },
+    {
+        id: 2,
+        dishName: "Brucheta",
+        price: "R 59.99",
+        image: require('../icons_assets/bruchetta.svg'),
+        description: "Our brucheta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."
+    },
+    {
+        id: 3,
+        dishName: "Lemon Desert",
+        price: "R 50.00",
+        image: require('../icons_assets/lemon dessert.jpg'),
+        description: "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined"
+    }
+]
